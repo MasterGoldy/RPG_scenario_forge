ef1d8124edf76a84a23b47ea725e39ff848042ae
MasterGoldy <MasterGoldCold@yandex.ru>
2026-01-11 12:27:54 +0500

    Add environment configuration

---
+++ env.py
@@ -0,0 +1,12 @@
+# Django Settings
+SECRET_KEY='oz09hk30@al37^(8w257@la54@a%b^!525n20ifk)a)w8yry=t'
+DEBUG=True
+ALLOWED_HOSTS= ['localhost', '127.0.0.1']
+
+# Database
+DATABASE_URL='sqlite:///db.sqlite3'
+
+# External APIs
+DND_API_URL='https://www.dnd5eapi.co/api'
+
+#MasterGoldy.pythonanywhere.com
